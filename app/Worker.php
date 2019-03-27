2e5ff7a5ea91177224b9c2f6389f6a90ab7ec5cb
Сергей <sh.ope@ukr.net>
2019-03-27 16:59:21 +0300

    Update Worker.php

--- app/Worker.php
+++ app/Worker.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Worker extends Model
 {
+    public function boss()
+    {
+        return $this->hasMany('Worker:class', 'id_boss');
+    }
 
+    public function worker()
+    {
+        return $this->belongsTo('Worker:class', 'id', 'id_boss');
+    }
 }
+
