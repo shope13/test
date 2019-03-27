b04e9edffd37836ee9fd823367611e503a3d40ca
Сергей <sh.ope@ukr.net>
2019-03-27 16:59:22 +0300

    Update 2019_03_27_073829_create-workers-table.php

--- database/migrations/2019_03_27_073829_create-workers-table.php
+++ database/migrations/2019_03_27_073829_create-workers-table.php
@@ -14,14 +14,16 @@ class CreateWorkersTable extends Migration
     public function up()
     {
         Schema::create('workers', function (Blueprint $table) {
-            $table->bigIncrements('id')->autoIncrement()->unique();
+            $table->bigIncrements('id')->unsigned();
             $table->string('name');
             $table->string('post');
             $table->date('d_of_emp');
             $table->integer('salary');
-            $table->integer('id_boss')-> nullable();
+            $table->bigInteger('id_boss')->unsigned()-> nullable();
             $table->timestamps();
+        });
 
+        Schema::table('workers', function (Blueprint $table){
             $table->foreign('id_boss')->references('id')->on('workers')->onDelete('set null');
         });
 
