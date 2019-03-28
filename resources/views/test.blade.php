9b12860efb4655d6c5241af919e1caa0cedce77a
Сергей <sh.ope@ukr.net>
2019-03-28 08:31:34 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,5 +3,12 @@
 @section('content')
 <div class="container">
 <h1>Home for all users</h1>
+
+        @foreach ($workers as $worker)
+            {{ $worker->name}}
+        {{$worker->post}}
+        @endforeach
+
+    {{ $workers->links() }}
 </div>
 @endsection
