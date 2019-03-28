c2411f9077843e8c50e7e2db08a81506340d3942
Сергей <sh.ope@ukr.net>
2019-03-28 16:59:21 +0300

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Worker::create(
-            array(
-                'name' => 'John',
-                'post' => 'Boss',
-                'd_of_emp' => '2011-11-11',
-                'salary' => 11
 
-                ));
     }
 }
