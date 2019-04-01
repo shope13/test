ac3825baf85c94adae32626fc24479baa6a2b654
Сергей <sh.ope@ukr.net>
2019-04-01 15:09:45 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,39 +4,44 @@
 <div class="container">
 <h1>Home for all users</h1>
 
+    {{--@foreach($workers as $worker)--}}
 
-<!-- example -->
-<ul>
 
-    @foreach ($workers as $worker)
 
-        <li>
-            @if($worker->parent_id==NULL)
-                <span class="caret">
-            {{ $worker->name}}
-        ({{ $worker->post}})</span>
-            @endif
-    <ul class="nested">
-      <li>Саббосс</li>
-      <li><span class="caret">Саббосс 2 уровня</span>
-        <ul class="nested">
-          <li>Ну и так далее</li>
-          <li><span class="caret">Это просто для примера</span>
-            <ul class="nested">
-              <li>Примера </li>
-            </ul>
-          </li>
-        </ul>
-      </li>
+
+
+
+    <ul>
+
+        @foreach ($workers as $worker)
+
+            <li>
+            <li><span class="caret">
+                BOSSSSSSSS
+                    {{--@include('worker')--}}
+                </span>
+                <ul class="nested">
+                    <li>Саббосс</li>
+                    <li><span class="caret">Саббосс 2 уровня</span>
+                        <ul class="nested">
+                            <li>Ну и так далее</li>
+                            <li><span class="caret">Это просто для примера</span>
+                                <ul class="nested">
+                                    <li>Примера </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        @endforeach
     </ul>
-  </li>
-    @endforeach
-</ul>
 {{ $workers->links() }}
 
 <!-- /example -->
-<br/>
-<hr/>
-<br/>
+    <br/>
+    <hr/>
+    <br/>
 </div>
+
 @endsection
