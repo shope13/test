e67f82fc09c6616db1fb16f8f0e0081423cf5316
Сергей <sh.ope@ukr.net>
2019-04-01 15:09:41 +0300

    Update Worker.php

--- app/Worker.php
+++ app/Worker.php
@@ -4,16 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed worker
+ * @method static find(int $int)
+ */
 class Worker extends Model
 {
+
+
+
+
     public function boss()
     {
-        return $this->hasMany(Worker::class, 'parent_id');
+        return $this->belongsTo(Worker::class, 'parent_id');
     }
 
-    public function worker()
+    public function workerBoss()
     {
-        return $this->belongsTo(Worker::class, 'id');
+        return $this->hasMany(Worker::class, 'id');
     }
 }
 
