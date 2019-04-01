5fcf294b6b337adb9ee7ecb97ba498a500a21656
Сергей <sh.ope@ukr.net>
2019-04-01 15:09:43 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,35 @@
 @section('content')
     <div class="container">
         <h1>List for auth</h1>
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Post</th>
+                <th scope="col">Date of employment</th>
+                <th scope="col">Salary</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($workers as $worker)
+            <tr>
+
+                <td>{{$worker->id}}</td>
+                <td>{{$worker->name}}</td>
+                <td>{{$worker->post}}</td>
+                <td>{{$worker->DateEmp}}</td>
+                <td>{{$worker->salary}}</td>
+
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+        {{--@foreach ($workers as $worker)--}}
+            {{----}}
+
+        {{--@endforeach--}}
     </div>
 @endsection
