eab89945e3958a8b230400377993e298d2e642f3
Сергей <sh.ope@ukr.net>
2019-04-01 15:09:31 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Worker;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $workers = Worker::paginate(10000);
+        return view('home',['workers' => $workers]);
     }
 }
