a359955e2a3be3c4e5842eb45aa7066d3ec9640b
Сергей <sh.ope@ukr.net>
2019-04-02 15:45:17 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,27 +3,14 @@
 @section('content')
 <div class="container">
 <h1>Employee hierarchy</h1>
-
-    {{--@foreach($workers as $worker)--}}
-
-
-
-
-
-
     <ul>
-
         @foreach ($workers as $worker)
             <li>
                     @include('worker')
-
             </li>
         @endforeach
     </ul>
 {{ $workers->links() }}
-
-<!-- /example -->
-
 </div>
 
 @endsection
