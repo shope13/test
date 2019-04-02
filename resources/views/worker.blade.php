54d814956d9feda608c5bfc049ea319faa1fc400
Сергей <sh.ope@ukr.net>
2019-04-02 12:00:13 +0300

    Update worker.blade.php

--- resources/views/worker.blade.php
+++ resources/views/worker.blade.php
@@ -1,13 +1,13 @@
 @if($worker)
+    <span class="caret">
         {{$worker->name}}
         {{$worker->post}}
+        </span>
+{{--    @if($worker->boss->implode('parent_id', ', ') == $worker->id))--}}
+    <ul class="nested">
 
-    @if(count($worker->workerBoss) > 0)
-        <ul>
             @foreach($worker->workerBoss as $emp)
                <li> @include('worker', ['worker' => $emp])</li>
                     @endforeach
         </ul>
     @endif
-@endif
-
