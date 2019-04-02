2aef55df5b3b553df1480bbf78848a1367aa45ad
Сергей <sh.ope@ukr.net>
2019-04-02 14:49:50 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -16,28 +16,14 @@
         @foreach ($workers as $worker)
             <li>
                     @include('worker')
-                <ul class="nested">
-                    <li>Саббосс</li>
-                    <li><span class="caret">Саббосс 2 уровня</span>
-                        <ul class="nested">
-                            <li>Ну и так далее</li>
-                            <li><span class="caret">Это просто для примера</span>
-                                <ul class="nested">
-                                    <li>Примера </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+
             </li>
         @endforeach
     </ul>
 {{ $workers->links() }}
 
 <!-- /example -->
-    <br/>
-    <hr/>
-    <br/>
+
 </div>
 
 @endsection
