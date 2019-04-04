cc9cd2dd4ed6cc25894cc2afa15f897d6cf12f03
Сергей <sh.ope@ukr.net>
2019-04-04 10:03:29 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-<h1>Employee hierarchy</h1>
-    <ul>
-        @foreach ($workers as $worker)
-            <li>
+    <div class="container">
+        <h1>Employee hierarchy</h1>
+        <ul>
+
+            @foreach (\App\Worker::with('workerBoss')->whereNull('parent_id')->get() as $worker)
+                <li>
                     @include('worker')
-            </li>
-        @endforeach
-    </ul>
-{{ $workers->links() }}
-</div>
+                </li>
+            @endforeach
+        </ul>
+        {{ $workers->links() }}
+    </div>
 
 @endsection
