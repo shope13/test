f70d8863856ec854aa05adc4e34c5a5917283246
Сергей <sh.ope@ukr.net>
2019-04-08 15:54:56 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script type="text/javascript" src="jquery.js"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
