48f6c866fb5da24bd7537dd105d50b3cca8992a6
Сергей <sh.ope@ukr.net>
2019-04-08 15:54:54 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,19 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
+    <script>
+
+        $('body').on("click", ".parent", function (e) {
+            const element = this;
+            $.ajax({
+                url: ,
+                type: "POST",
+                data: {
+                    name: "Ne pidor!"
+                },
+                success: function (response) {
+                    $(element).append('<ul><li>' + response.name + '</li></ul>')
+                }
+            });
+
+
+        });
+    </script>
     <div class="container">
         <h1>Employee hierarchy</h1>
-
         <ul>
+            @foreach(\App\Worker::with('employees')->whereNull('parent_id')->get() as $worker)
+            <li>
+                <div class='parent'>
 
+                    {{$worker->name}}
+                        ({{$worker->post}})
 
-            @foreach (\App\Worker::with('workerBoss')->whereNull('parent_id')->get() as $worker)
-                <li>
-                    @include('worker')
-                </li>
-            @endforeach
+                </div>
+            </li>
+                @endforeach
         </ul>
-        {{ $workers->links() }}
     </div>
 
 @endsection
