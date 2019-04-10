09ca25530943fa5cdb95be1453ebc7a84bdc3647
Сергей <47629060+shope13@users.noreply.github.com>
2019-04-10 17:08:35 +0300

    Update employeesScript.js

--- public/js/employeesScript.js
+++ public/js/employeesScript.js
@@ -2,20 +2,16 @@
 $('body').on("click", ".parent", function (e) {
     const element = $(this).attr('data-id');
     const el = this;
-
     e.stopPropagation();
     $.ajax({
         url: "http://127.0.0.1:8000/"+element,
         type: "GET",
         dataType: "json",
         success: function (response) {
-
                 jQuery.each(response, function (r) {
-
                     $(el).append('<ul class="list-group"><li class="list-group-item"><div class="parent" data-id="' + response[r].id + '">'
                         + response[r].id + ' ' + response[r].name + ' ' + '(' + response[r].post + ')' + ' ' + response[r].parent_id
                         + '</li></div></ul>')
-
                 });
             }
 
