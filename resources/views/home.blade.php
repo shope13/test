79f6fc1a1238ae4232192316adee9afc87cb672e
Сергей <47629060+shope13@users.noreply.github.com>
2019-04-10 17:08:39 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<script src="js/search.js"></script>
 @section('content')
     <div class="container">
         <h1>List of employees</h1>
@@ -8,7 +8,6 @@
             <div class="col-md-3">
                 <form action="#" method="post">
                     <div class="input-group">
-                        <!-- USE TWITTER TYPEAHEAD JSON WITH API TO SEARCH -->
                         <input class="form-control" id="system-search" name="q" placeholder="Search for" required>
                         <span class="input-group-btn">
                         <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
