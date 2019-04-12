c333f8597c3cd13bc2f358ae3f0220856bef0226
Сергей <47629060+shope13@users.noreply.github.com>
2019-04-13 00:52:11 +0300

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,20 +3,16 @@
 @section('content')
     <div class="container">
         <h1>Employee hierarchy</h1>
-        <ul class="list-group">
+        <ul class="list-group" id="list">
             @foreach($workers as $worker)
             <li class="list-group-item">
-                <div class="parent" data-id="{{$worker->id}}">
-                    {{$worker->id}}
+                <div class="parent"  data-id="{{$worker->id}}">
                     {{$worker->name}}
                         ({{$worker->post}})
-                    {{$worker->parent_id}}
-
                 </div>
                 @endforeach
-            {{$workers->render()}}
         </ul>
-
+        {{$workers->render()}}
     </div>
 
 @endsection
