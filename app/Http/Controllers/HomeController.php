e6466bfa4f7ee5986ddf0365f5046290ac13d0ef
Сергей <47629060+shope13@users.noreply.github.com>
2019-05-07 17:07:10 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Worker;
+use Validator;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,9 +22,113 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        $workers = Worker::paginate(10000);
-        return view('home',['workers' => $workers]);
+
+        $request->session()->put('search', $request
+            ->has('search') ? $request->get('search') : ($request->session()
+            ->has('search') ? $request->session()->get('search') : ''));
+
+        $request->session()->put('field', $request
+            ->has('field') ? $request->get('field') : ($request->session()
+            ->has('field') ? $request->session()->get('field') : 'id'));
+
+        $request->session()->put('sort', $request
+            ->has('sort') ? $request->get('sort') : ($request->session()
+            ->has('sort') ? $request->session()->get('sort') : 'asc'));
+
+        $workers = new Worker();
+        $workers = $workers->where('id', 'like', '%'.$request->session()->get('search') . '%')
+            ->orWhere('name', 'like', '%'.$request->session()->get('search') . '%')
+            ->orWhere('post', 'like', '%'.$request->session()->get('search') . '%')
+            ->orWhere('DateEmp', 'like', '%'.$request->session()->get('search') . '%')
+            ->orWhere('salary', 'like', '%'.$request->session()->get('search') . '%')
+            ->with('boss')
+            ->orderBy($request->session()->get('field'), $request->session()->get('sort'))
+            ->paginate(100);
+
+        if ($request->ajax()) {
+            return view('home.home', ['workers' => $workers]);
+        }else{
+            return view('home.ajax', ['workers' => $workers]);
+        }
     }
+
+    public function create(Request $request)
+    {
+        if ($request->isMethod('get'))
+            return view('home.form');
+
+        $rules = [
+            'name' => 'required',
+            'post' => 'required',
+            'DateEmp' => 'required',
+            'salary' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails())
+            return response()->json([
+                'fail' =>true,
+                'errors' => $validator->errors()
+            ]);
+
+        $workers = new Worker();
+        $workers->name = $request->name;
+        $workers->post = $request->post;
+        $workers->DateEmp = $request->DateEmp;
+        $workers->salary = $request->salary;
+        $workers->save();
+
+        return response()->json([
+            'fail' => false,
+            'redirect_url' => url('home')
+        ]);
+    }
+
+    public function show(Request $request, $id)
+    {
+        if($request->isMethod('get')) {
+            return view('home.detail',['workers' => Worker::find($id)]);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        if ($request->isMethod('get'))
+            return view('home.form',['workers' => Worker::find($id)]);
+
+        $rules = [
+            'name' => 'required',
+            'post' => 'required',
+            'DateEmp' => 'required',
+            'salary' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails())
+            return response()->json([
+                'fail' =>true,
+                'errors' => $validator->errors()
+            ]);
+
+        $workers = Worker::find($id);
+        $workers->name = $request->name;
+        $workers->post = $request->post;
+        $workers->DateEmp = $request->DateEmp;
+        $workers->salary = $request->salary;
+        $workers->save();
+
+        return response()->json([
+            'fail' => false,
+            'redirect_url' => url('home')
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        Worker::destroy($id);
+        return redirect('home');
+    }
+
 }
