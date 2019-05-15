21f103beaedc8fb5e96768ebc1b2fc70df30be3b
Сергей <47629060+shope13@users.noreply.github.com>
2019-05-15 14:48:44 +0300

    Update form.blade.php

--- resources/views/home/form.blade.php
+++ resources/views/home/form.blade.php
@@ -5,13 +5,13 @@
         @if(isset($workers))
             {!! Form::model($workers,['method'=>'put','id'=>'frm']) !!}
         @else
-            {!! Form::open(['id'=>'frm']) !!}
+            {!! Form::open(['id'=>'frm','files' => 'true']) !!}
         @endif
         <div class="form-group row required">
             {!! Form::label("name","Name",["class"=>"col-form-label col-md-3 col-lg-2"]) !!}
             <div class="col-md-8">
                 {!! Form::text("name",null,["class"=>"form-control".($errors->has('name')?" is-invalid":""),"autofocus",'placeholder'=>'Name']) !!}
-                <span id="error-title" class="invalid-feedback"></span>
+                <span id="error-name" class="invalid-feedback"></span>
             </div>
         </div>
 
@@ -19,7 +19,7 @@
             {!! Form::label("post","Post",["class"=>"col-form-label col-md-3 col-lg-2"]) !!}
             <div class="col-md-8">
                 {!! Form::text("post",null,["class"=>"form-control".($errors->has('post')?" is-invalid":""),'placeholder'=>'Post']) !!}
-                <span id="error-description" class="invalid-feedback"></span>
+                <span id="error-post" class="invalid-feedback"></span>
             </div>
         </div>
 
@@ -27,7 +27,7 @@
             {!! Form::label("DateEmp","Date of Employee",["class"=>"col-form-label col-md-3 col-lg-2"]) !!}
             <div class="col-md-8">
                 {!! Form::date("DateEmp",null,["class"=>"form-control".($errors->has('DateEmp')?" is-invalid":""),'placeholder'=>'Date of Employee']) !!}
-                <span id="error-description" class="invalid-feedback"></span>
+                <span id="error-DateEmp" class="invalid-feedback"></span>
             </div>
         </div>
 
@@ -35,17 +35,19 @@
             {!! Form::label("salary","Salary",["class"=>"col-form-label col-md-3 col-lg-2"]) !!}
             <div class="col-md-8">
                 {!! Form::text("salary",null,["class"=>"form-control".($errors->has('salary')?" is-invalid":""),'placeholder'=>'Salary']) !!}
-                <span id="error-description" class="invalid-feedback"></span>
+                <span id="error-salary" class="invalid-feedback"></span>
             </div>
         </div>
 
-        {{--<div class="form-group row required">--}}
-            {{--{!! Form::label("boss","Boss",["class"=>"col-form-label col-md-3 col-lg-2"]) !!}--}}
-            {{--<div class="col-md-8">--}}
-                {{--{!! Form::select("boss",null,["class"=>"form-control".($errors->has('boss')?" is-invalid":""),'placeholder'=>'Boss']) !!}--}}
-                {{--<span id="error-description" class="invalid-feedback"></span>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+        <div class="form-group row">
+            {!! Form::label('parent_id', 'Select employee boss',['class' => 'col-form-label col-md-3 col-lg-2']) !!}
+            <div class="col-md-8">
+            {!! Form::text("parent_id",null,["class"=>"form-control".($errors->has('parent_id')?" is-invalid":""),'placeholder'=>'Boss name', "autocomplete"=>"off"]) !!}
+            <ul class="boss-select list-group">
+            </ul>
+                <span id="error-parent_id" class="invalid-feedback"></span>
+            </div>
+        </div>
 
         <div class="form-group row">
             <div class="col-md-3 col-lg-2"></div>
