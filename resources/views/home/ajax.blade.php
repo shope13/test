4db56fb97e458c063809d2830490d7afa8b6875b
Сергей <47629060+shope13@users.noreply.github.com>
2019-05-15 14:48:41 +0300

    Update ajax.blade.php

--- resources/views/home/ajax.blade.php
+++ resources/views/home/ajax.blade.php
@@ -1,27 +1,4 @@
 @extends('layouts.app')
-
-@section('css')
-    <style>
-        .loading {
-            background: lightgrey;
-            padding: 15px;
-            position: fixed;
-            border-radius: 4px;
-            left: 50%;
-            top: 50%;
-            text-align: center;
-            margin: -40px 0 0 -50px;
-            z-index: 2000;
-            display: none;
-        }
-
-        .form-group.required label:after {
-            content: " *";
-            color: red;
-            font-weight: bold;
-        }
-    </style>
-@endsection
 @section('content')
     <div id="content">
         @include('home/home')
@@ -31,8 +8,4 @@
         <br>
         <span>Loading</span>
     </div>
-@endsection
-
-@section('js')
-    <script src="{{ asset('js/ajaxcrud.js') }}"></script>
 @endsection
